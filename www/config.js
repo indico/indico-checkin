f17a50ec2515205395a35ff65bd00e588c16a18c
Bernard Kolobara <bernard.kolobara@gmail.com>
2013-10-15 16:52:17 +0200

    [IMP] Comment the config.js file

--- www/config.js
+++ www/config.js
@@ -1,9 +1,16 @@
 var options = {
-    baseUrl: 'http://pcituds01.cern.ch:8000/indico',
+    // baseUrl should point to Indico's main URL
+    baseUrl: 'http://example.com:8000/indico',
+    // callbackUrl should NOT be changed. It is used as a trigger in the OAuth
+    // process. The value is not important but should not match an existing URL
     callbackUrl: 'http://callback.check',
-    requestTokenUrl: 'http://pcituds01.cern.ch:8000/indico/oauth/request_token',
-    authorizationUrl: 'http://pcituds01.cern.ch:8000/indico/oauth/authorize',
-    accessTokenUrl: 'http://pcituds01.cern.ch:8000/indico/oauth/access_token',
+    // requestTokenUrl is used by OAuth to get the request token
+    requestTokenUrl: 'http://example.com:8000/indico/oauth/request_token',
+    // authorizationURL is used by OAuth during the authentication
+    authorizationUrl: 'http://example.com:8000/indico/oauth/authorize',
+    // accessTokenUrl is used by OAuth to obtain the access token
+    accessTokenUrl: 'http://example.com:8000/indico/oauth/access_token',
+    // The consumer key and secret used by OAuth
     consumerKey: 'iHcIIWXT1xONeu0UPTDGdVCNCxdgOg1NLTuDCxmL',
     consumerSecret: 'OnpK2NxJXTvSQh2xboEf5Xyz5hTQZ4Ve9TvOSaY6'
 };
