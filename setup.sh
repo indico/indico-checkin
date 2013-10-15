6eb4e0ed6c170f4e1089e56673e243bb3bb5b2c1
Bernard Kolobara <bernard.kolobara@gmail.com>
2013-10-15 14:20:30 +0200

    [FTR] Add setup file for local build

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+textReset=$(tput sgr0)
+textGreen=$(tput setaf 2)
+message_info () {
+  echo "${textGreen}[CheckIn mobile]${textReset} $1"
+}
+
+message_info "Creating necessary directories..."
+mkdir plugins
+mkdir platforms
+
+message_info "Adding plugins..."
+# If using cordova, change to: cordova plugin add
+phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-splashscreen.git
+phonegap local plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-inappbrowser.git
+phonegap local plugin add https://github.com/wildabeast/BarcodeScanner.git
