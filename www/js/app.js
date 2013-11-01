f3c5611c86dc2511b8305bad4c6d7f52f1e5ca5e
Alberto Resco Perez <alberto.resco.perez@cern.ch>
2013-11-01 16:30:34 +0100

    [IMP] Added License GPLv3

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,19 @@
+/* This file is part of Indico check-in.
+ * Copyright (C) 2002 - 2013 European Organization for Nuclear Research (CERN).
+ *
+ * Indico is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Indico is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Indico check-in; if not, see <http://www.gnu.org/licenses/>.
+ */
 
 var app = {
     initialize: function () {
@@ -13,6 +29,5 @@ var app = {
     },
 
     receivedEvent: function (id) {
-        //angular.bootstrap(document, ['Checkinapp']);
     }
 };
